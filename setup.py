c689e228e0bbe9140c7732e6f213ac7396676da0
Christian Johnson <christian.johnson@deepsight.de>
2020-11-09 10:27:02 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
 
+
+setup(
+    name="fast-keywords",
+    version="0.0.1",
+    description="Fast keyword identification with n-gram vector string matching.",
+    url="",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==0.25.1"
+    ]
+)
